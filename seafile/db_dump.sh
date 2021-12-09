6cb9ab55f2453b2b9cfa7571243f686b6766ed8f
Lukas Patzke <lukas@patzke.me>
2021-12-09 17:08:50 +0000

    fix seafile sb dump

--- seafile/db_dump.sh
+++ seafile/db_dump.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-docker exec -t seafile-mysql sh -c 'exec mysqldump -p"$MYSQL_ROOT_PASSWORD" --opt ccnet-db' > /home/lukas/srv/backup/dumps/seafile_ccnet-db_dump.sql
-docker exec -t seafile-mysql sh -c 'exec mysqldump -p"$MYSQL_ROOT_PASSWORD" --opt seafile-db' > /home/lukas/srv/backup/dumps/seafile_seafile-db_dump.sql
-docker exec -t seafile-mysql sh -c 'exec mysqldump -p"$MYSQL_ROOT_PASSWORD" --opt seahub-db' > /home/lukas/srv/backup/dumps/seafile_seahub-db_dump.sql
+docker exec -t seafile-mysql sh -c 'exec mysqldump -p"$MYSQL_ROOT_PASSWORD" --opt ccnet_db' > /home/lukas/srv/backup/dumps/seafile_ccnet-db_dump.sql
+docker exec -t seafile-mysql sh -c 'exec mysqldump -p"$MYSQL_ROOT_PASSWORD" --opt seafile_db' > /home/lukas/srv/backup/dumps/seafile_seafile-db_dump.sql
+docker exec -t seafile-mysql sh -c 'exec mysqldump -p"$MYSQL_ROOT_PASSWORD" --opt seahub_db' > /home/lukas/srv/backup/dumps/seafile_seahub-db_dump.sql
