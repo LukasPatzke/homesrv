7b0ebb6213c72b1df1f61db84c9d92331671a9b4
Lukas Patzke <lukas@patzke.me>
2022-08-15 15:47:14 +0000

    Delete old influxdb backups

--- smarthome/db_dump.sh
+++ smarthome/db_dump.sh
@@ -1,3 +1,5 @@
 #/bin/bash
+# Remove all old backups
+rm -f /home/lukas/srv/smarthome/backup/*
 
 docker exec -t sensor_influxdb sh -c 'influx backup /backup -t "$INFLUXDB_ADMIN_PASSWORD"'
--- weatherflow-dashboards-aio/db_dump.sh
+++ weatherflow-dashboards-aio/db_dump.sh
@@ -1,3 +1,5 @@
 #/bin/bash
+# Remove all old backups
+rm -f /home/lukas/srv/weatherflow-dashboards-aio/backup/*
 
 docker exec -t wxfdashboardsaio_influxdb influxd backup -portable /backup
